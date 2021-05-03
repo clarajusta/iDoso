2c85750f309b177059c3a389103f699012b0710a
clarajusta <clarajusta31@gmail.com>
2021-05-03 00:57:39 -0400

    eventos do dia com assiste de voz, porem em ingles

---
+++ ingles_eventos.py
@@ -0,0 +1,143 @@
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import os
+import time
+import pyttsx3
+import speech_recognition as sr
+import pytz
+
+
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+MONTHS = ["january", "february", "march", "april", "may", "june","july", "august", "september","october","november", "december"]
+DAYS = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+DAY_EXTENTIONS = ["rd", "th", "st", "nd"]
+
+def speak(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        said = ""
+
+        try:
+            said = r.recognize_google(audio)
+            print(said)
+        except Exception as e:
+            print("Exception: " + str(e))
+
+    return said
+
+
+def authenticate_google():
+    credentials = pickle.load(open("token.pkl", "rb"))
+    service = build("calendar", "v3", credentials=credentials)
+
+    return service
+
+
+def get_events(day, service):
+    date = datetime.datetime.combine(day, datetime.datetime.min.time())
+    end_date = datetime.datetime.combine(day, datetime.datetime.max.time())
+    utc = pytz.UTC
+    date = date.astimezone(utc)
+    end_date = end_date.astimezone(utc)
+
+    events_result = service.events().list(calendarId='primary', timeMin=date.isoformat(), timeMax=end_date.isoformat(),
+                                        singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        speak('No upcoming events found.')
+    else:
+        speak(f"You have {len(events)} events on this day.")
+
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            print(start, event['summary'])
+            start_time = str(start.split("T")[1].split("-")[0])
+            if int(start_time.split(":")[0]) < 12:
+                start_time = start_time + "am"
+            else:
+                start_time = str(int(start_time.split(":")[0])-12)
+                start_time = start_time + "pm"
+
+            speak(event["summary"] + " at " + start_time)
+
+
+def get_date(text):
+    text = text.lower()
+    today = datetime.date.today()
+
+    if text.count("today") > 0:
+        return today
+
+    day = -1
+    day_of_week = -1
+    month = -1
+    year = today.year
+
+    for word in text.split():
+        if word in MONTHS:
+            month = MONTHS.index(word) + 1
+        elif word in DAYS:
+            day_of_week = DAYS.index(word)
+        elif word.isdigit():
+            day = int(word)
+        else:
+            for ext in DAY_EXTENTIONS:
+                found = word.find(ext)
+                if found > 0:
+                    try:
+                        day = int(word[:found])
+                    except:
+                        pass
+
+    if month < today.month and month != -1:  
+        year = year+1
+
+    
+    if month == -1 and day != -1:  
+        if day < today.day:
+            month = today.month + 1
+        else:
+            month = today.month
+
+    
+    if month == -1 and day == -1 and day_of_week != -1:
+        current_day_of_week = today.weekday()
+        dif = day_of_week - current_day_of_week
+
+        if dif < 0:
+            dif += 7
+            if text.count("next") >= 1:
+                dif += 7
+
+        return today + datetime.timedelta(dif)
+
+    if day != -1:  
+        return datetime.date(month=month, day=day, year=year)
+
+
+
+service = authenticate_google()
+print("Start")
+text = get_audio()
+
+CALENDAR_STRS = ["what do i have", "do i have plans", "am i busy"]
+for phrase in CALENDAR_STRS:
+    if phrase in text.lower():
+        date = get_date(text)
+        if date:
+            get_events(date, service)
+        else:
+            speak("Please Try Again")
