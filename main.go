402431afd2b911612d35636cfb156255485f006e
AleMaiaF <alessandra.maia.freire@gmail.com>
2021-05-10 23:32:42 -0300

    removidos arquivos extras, limpeza feita

--- main.go
+++ main.go
@@ -1,35 +1,6 @@
 package main
 
-import (
-    "bufio"
-    "fmt"
-    "io"
-    "os/exec"
-)
-
 func main() {
-    cmd := exec.Command("python", "speech_to_text.py")
-    stdout, err := cmd.StdoutPipe()
-    if err != nil {
-        panic(err)
-    }
-    stderr, err := cmd.StderrPipe()
-    if err != nil {
-        panic(err)
-    }
-    err = cmd.Start()
-    if err != nil {
-        panic(err)
-    }
-
-    go copyOutput(stdout)
-    go copyOutput(stderr)
-    cmd.Wait()
-}
-
-func copyOutput(r io.Reader) {
-    scanner := bufio.NewScanner(r)
-    for scanner.Scan() {
-		fmt.Printf("%s\n ", scanner.Text())
-    }
+	//fazer o arquivo main
+	// aqui vai ficar o arquivo que pega as strings de frase_ouvida.JSON e interpreta
 }
