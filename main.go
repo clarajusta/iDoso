c876952792606723a93f72e842baccf046f2268c
AleMaiaF <alessandra.maia.freire@gmail.com>
2021-05-12 22:50:08 -0300

    perguntas de data e hora adicionadas

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
     "os/exec"
     "regexp"
 	"strings"
+	"time"
+	"github.com/goodsign/monday"
 )
 
 var frase string = "..."
@@ -61,6 +63,7 @@ func main() {
 		//Encontrando a palavra-chave MARA
         matched, err := regexp.MatchString("MARA", frase)
         if matched{
+			exec.Command("/bin/bash", "speech.sh", "Você me chamou?").Output()
 			mara = true
             fmt.Println("Chamou? \n     V\n      L('o' )/    \n")
 			fmt.Println("Mara_1: ",mara)
@@ -72,7 +75,26 @@ func main() {
 func funcLoop(comando string) {
 	fmt.Println("Executando função", comando)
 	if strings.Contains(comando, "BOM DIA"){
+		exec.Command("/bin/bash", "speech.sh", "Bom dia para você também!").Output()
 		fmt.Println("Bom dia para você também *(^-^)*")
+	}
+	if strings.Contains(comando, "CONTE") && strings.Contains(comando, "PIADA"){
+		exec.Command("/bin/bash", "speech.sh", "Aqui está uma piada para você. .Porquê a velhinha não usa relógio? . Porquê ela é senhora!").Output()
+		fmt.Println("(/'v')/ TA-DAH!!\n")
+	}
+	if strings.Contains(comando, "HORAS SÃO"){
+		data := time.Now()
+		data_en := "Agora são 15:04 do dia 2. de January de 2006"
+		data_PtBR := monday.Format(data, data_en,monday.LocalePtBR)
+		exec.Command("/bin/bash", "speech.sh", data_PtBR).Output()
+		fmt.Println(data_PtBR)
+	}
+	if strings.Contains(comando, "DIA")&&strings.Contains(comando, "HOJE"){
+		data := time.Now()
+		data_en := "Hoje é Monday , 2. de January de 2006"
+		data_PtBR := monday.Format(data, data_en,monday.LocalePtBR)
+		exec.Command("/bin/bash", "speech.sh", data_PtBR).Output()
+		fmt.Println(data_PtBR)
 	}
 	mara = false
 	main()
